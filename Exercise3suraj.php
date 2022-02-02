a5e03e4f1a1434eaea0147299b56982e104463a4
KhareSuraj <91323702+KhareSuraj@users.noreply.github.com>
2022-02-02 10:01:01 +0200

    Update Exercise3suraj.php

--- Exercise3suraj.php
+++ Exercise3suraj.php
@@ -1,3 +1,78 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 1 </title>
+</head>
+<style>
+    body{
+        color: #4169E1;
+        font-family:Times New Roman (serif);
 
-?>
+    }
+    table, th, td {
+        border: 2px solid orange;
+        border-collapse: collapse;
+        border:color:#808080
+        padding: 10px 5px;
+    }
+    
+    h1{
+        color:#CD853F;
+        font-size: 20px;
+        margin-bottom: 40px;
+        background:#F0F8FF;
+
+    }
+    h2{
+        color:#191970;
+        font-size: 18px;
+        margin-bottom: 5px;
+        background:#F0F8FF;  
+    }
+    h3{
+        color:#4682B4;
+        font-size: 15px;
+        margin-bottom: 5px;
+        background:#F0F8FF; 
+    }
+    div{
+        max: width 1000px;
+        width:80%
+        margin: 30px auto;
+    }
+    
+</style>
+
+<body>
+    <div>
+
+    <h1>Exercise 3 <br> Suraj Kharel</h2>
+
+    <h2>Task 1 </h2>
+    <h3>Use IF else
+
+    1. Write a script that gets the current month and prints one of the following responses, depending on whether it's August or not:
+
+    It's August, so it's still holiday.
+    Not August, This is Month-name so i don't have any holidays
+
+    You can use date(F) function to get the current month name</h3>
+    <?php
+    
+    ?>
+
+    <h2> Task 2 </h2>
+    <h3> Assign color red to a variable name $color and check to print one the following responses using if else statement 
+
+        The color is red. 
+
+        The color is not red. </h3>
+    <?php
+   
+    ?>
+</body>
+    </div>
+</html>
